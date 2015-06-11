c28437458b013498acf3bca18bb7e85e1b9a64c5
Tom Fogg <tom@canobe.com>
2015-06-11 11:35:05 +0100

    smaller arrows

--- shark.js
+++ shark.js
@@ -49,30 +49,30 @@ d3.json("nodes.json", function(error, graph) {
     .attr({
         'id':function(d,i) { return 'arrow'+i;},
         'class': 'arrow',
-        'viewBox':'-0 -5 10 10',
-        'refX': 10,
+        'viewBox':'-0 -3 6 6',
+        'refX': 3,
         'refY':0,
         'orient':'auto',
-        'markerWidth':10,
-        'markerHeight':10,
+        'markerWidth':6,
+        'markerHeight':6,
         'xoverflow':'visible'
     }).append('svg:path')
     .style("fill", function(d) { var p = ports.indexOf(d[3]); return p == -1 ? color2(0) : color2(p+1); })
-    .attr('d', 'M 0,-5 L 10 ,0 L 0,5');
+    .attr('d', 'M 0,-3 L 6 ,0 L 0,3');
     defs.append('marker')
     .style("pointer-events", "none")
     .attr({
         'id':'shadowarrow',
         'class': 'shadowarrow',
-        'viewBox':'-0 -5 10 10',
-        'refX': 8,
+        'viewBox':'-0 -3 6 6',
+        'refX': 2.5,
         'refY':0,
         'orient':'auto',
-        'markerWidth':10,
-        'markerHeight':10,
+        'markerWidth':6,
+        'markerHeight':6,
         'xoverflow':'visible'
     }).append('svg:path')
-    .attr('d', 'M 0,-5 L 10 ,0 L 0,5');
+    .attr('d', 'M 0,-2 L 6 ,0 L 0,3');
     
     // add a circle for each host group
     var node = svg.selectAll('.node')
@@ -132,7 +132,7 @@ d3.json("nodes.json", function(error, graph) {
     // redraw the graph
     force.on("tick", function() {
         link.attr("d", function(d) {return "M"+d[0].x+","+d[0].y+"S"+d[1].x+","+d[1].y+" "+d[2].x+","+d[2].y;});
-        var soff = 2;
+        var soff = 0;
         shadowlink.attr("d", function(d) {return "M"+(d[0].x+soff)+","+(d[0].y+soff)+"S"+(d[1].x+soff)+","+(d[1].y+soff)+" "+(d[2].x+soff)+","+(d[2].y+soff);});
         node.attr("transform", function(d) { 
             return 'translate(' + [d.x, d.y] + ')'; 
